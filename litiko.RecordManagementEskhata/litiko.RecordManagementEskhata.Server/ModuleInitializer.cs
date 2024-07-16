0ec2c2fcadebdb749eb42da6cdbfc80ac0d64c8f
Tech84 <tech84@litiko.com>
2024-07-16 17:14:02 +0300

    Додав види документів для ОРД [Rx]

---
+++ litiko.RecordManagementEskhata/litiko.RecordManagementEskhata.Server/ModuleInitializer.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Domain.Initialization;
+
+namespace litiko.RecordManagementEskhata.Server
+{
+  public partial class ModuleInitializer
+  {
+
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      CreateDocumentKinds();
+    }
+    
+    public static void CreateDocumentKinds()
+    {
+      #region Settings
+      InitializationLogger.Debug("Init: Create document kinds.");
+      
+      var registrable = Sungero.Docflow.DocumentKind.NumberingType.Registrable;
+      var numerable = Sungero.Docflow.DocumentKind.NumberingType.Numerable;
+      var notNumerable = Sungero.Docflow.DocumentKind.NumberingType.NotNumerable;
+      
+      var actions = new Sungero.Domain.Shared.IActionInfo[] {
+        Sungero.Docflow.OfficialDocuments.Info.Actions.SendActionItem,
+        Sungero.Docflow.OfficialDocuments.Info.Actions.SendForFreeApproval,
+        Sungero.Docflow.OfficialDocuments.Info.Actions.SendForApproval,
+        Sungero.Docflow.OfficialDocuments.Info.Actions.SendForAcquaintance };
+      #endregion
+      
+      #region Приказ от имени Председателя Правления
+      Sungero.Docflow.PublicInitializationFunctions.Module.
+        CreateDocumentKind(litiko.RecordManagementEskhata.Resources.CharmanOrder,
+                           litiko.RecordManagementEskhata.Resources.CharmanOrder,
+                           registrable,
+                           Sungero.Docflow.DocumentKind.DocumentFlow.Inner,
+                           true,
+                           false,
+                           Constants.Module.DocumentTypeGuids.Order,
+                           actions,
+                           Constants.Module.DocumentKindGuids.CharmanOrder,
+                           true);
+      #endregion
+      
+      #region Приказ по утверждению нормативного документа
+      Sungero.Docflow.PublicInitializationFunctions.Module.
+        CreateDocumentKind(litiko.RecordManagementEskhata.Resources.NormativeOrder,
+                           litiko.RecordManagementEskhata.Resources.NormativeOrder,
+                           registrable,
+                           Sungero.Docflow.DocumentKind.DocumentFlow.Inner,
+                           true,
+                           false,
+                           Constants.Module.DocumentTypeGuids.Order,
+                           actions,
+                           Constants.Module.DocumentKindGuids.NormativeOrder,
+                           false);
+      #endregion
+      
+      #region Распоряжение по департаменту
+      Sungero.Docflow.PublicInitializationFunctions.Module.
+        CreateDocumentKind(litiko.RecordManagementEskhata.Resources.DepartmentalDirective,
+                           litiko.RecordManagementEskhata.Resources.DepartmentalDirective,
+                           registrable,
+                           Sungero.Docflow.DocumentKind.DocumentFlow.Inner,
+                           true,
+                           false,
+                           Constants.Module.DocumentTypeGuids.CompanyDirective,
+                           actions,
+                           Constants.Module.DocumentKindGuids.DepartmentalDirective,
+                           true);
+      #endregion
+
+      #region Приказ по филиалу
+      Sungero.Docflow.PublicInitializationFunctions.Module.
+        CreateDocumentKind(litiko.RecordManagementEskhata.Resources.BranchOrder,
+                           litiko.RecordManagementEskhata.Resources.BranchOrder,
+                           registrable,
+                           Sungero.Docflow.DocumentKind.DocumentFlow.Inner,
+                           true,
+                           false,
+                           Constants.Module.DocumentTypeGuids.Order,
+                           actions,
+                           Constants.Module.DocumentKindGuids.BranchOrder,
+                           false);
+      #endregion
+    }
+  }
+}
---
+++ litiko.RecordManagementEskhata/litiko.RecordManagementEskhata.Shared/ModuleConstants.cs
@@ -0,0 +1,36 @@
+using System;
+using Sungero.Core;
+
+namespace litiko.RecordManagementEskhata.Constants
+{
+  public static class Module
+  {
+    public static class DocumentTypeGuids
+    {
+      [Sungero.Core.Public]
+      public static readonly Guid CompanyDirective = Guid.Parse("264ada4e-b272-4ecc-a115-1246c9556bfa");
+      
+      [Sungero.Core.Public]
+      public static readonly Guid Order = Guid.Parse("9570e517-7ab7-4f23-a959-3652715efad3");
+    }
+    
+    public static class DocumentKindGuids
+    {
+      /// <summary> Приказ от имени Председателя Правления </summary>
+      [Sungero.Core.Public]
+      public static readonly Guid CharmanOrder = Guid.Parse("98ec419e-d7c1-4f62-8ab8-d0dfc0632c6b");
+      
+      /// <summary> Приказ по утверждению нормативного документа </summary>
+      [Sungero.Core.Public]
+      public static readonly Guid NormativeOrder = Guid.Parse("aad5fbea-26d6-42fb-a938-6f2b3c1952d8");
+      
+      /// <summary> Распоряжение по департаменту </summary>
+      [Sungero.Core.Public]
+      public static readonly Guid DepartmentalDirective = Guid.Parse("812ac7b2-f762-492a-9045-f97f364dc9f9");
+      
+      /// <summary> Приказ по филиалу </summary>
+      [Sungero.Core.Public]
+      public static readonly Guid BranchOrder = Guid.Parse("9018466b-9bdc-4179-8046-af55bbf1d624");
+    }
+  }
+}
