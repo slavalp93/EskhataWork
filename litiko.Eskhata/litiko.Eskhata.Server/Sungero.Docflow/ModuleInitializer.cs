49411699d1334d10c4882196ddb40a0eacbbcdc0
RomanovSV <slava@litiko.com>
2025-04-25 11:31:19 +0300

    +1 [Rx]

--- litiko.Eskhata/litiko.Eskhata.Server/Sungero.Docflow/ModuleInitializer.cs
+++ litiko.Eskhata/litiko.Eskhata.Server/Sungero.Docflow/ModuleInitializer.cs
@@ -9,5 +9,15 @@ namespace litiko.Eskhata.Module.Docflow.Server
 {
   public partial class ModuleInitializer
   {
+
+    public override bool IsModuleVisible()
+    {
+      //return base.IsModuleVisible();
+      
+      // "Ответственные за настройку регистрации"
+      return Users.Current.IncludedIn(Sungero.Docflow.PublicConstants.Module.RoleGuid.RegistrationManagersRole) ||        
+        // "Администраторы"
+        Users.Current.IncludedIn(Roles.Administrators);      
+    }
   }
 }
