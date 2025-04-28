e6a0f3cac7b14ec443977baf88fd7a9656bf5bc0
RomanovSV <slava@litiko.com>
2025-04-28 15:19:55 +0300

    Забрать права на изменение Видов документов у роли "Ответственные за настройку регистрации" [Rx]

--- litiko.Eskhata/litiko.Eskhata.Server/Sungero.Docflow/ModuleInitializer.cs
+++ litiko.Eskhata/litiko.Eskhata.Server/Sungero.Docflow/ModuleInitializer.cs
@@ -10,6 +10,22 @@ namespace litiko.Eskhata.Module.Docflow.Server
   public partial class ModuleInitializer
   {
 
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      base.Initializing(e);
+      
+      // Забрать права на изменение Видов документов у роли "Ответственные за настройку регистрации".
+      var registrationManagers = Roles.GetAll().SingleOrDefault(n => n.Sid == Sungero.Docflow.PublicConstants.Module.RoleGuid.RegistrationManagersRole);
+      if (registrationManagers != null)
+      {
+        if (Sungero.Docflow.DocumentKinds.AccessRights.IsGrantedDirectly(DefaultAccessRightsTypes.Change, registrationManagers))
+        {
+          Sungero.Docflow.DocumentKinds.AccessRights.Revoke(registrationManagers, DefaultAccessRightsTypes.Change);
+          Sungero.Docflow.DocumentKinds.AccessRights.Save();
+        }        
+      }
+    }
+
     public override bool IsModuleVisible()
     {
       //return base.IsModuleVisible();
