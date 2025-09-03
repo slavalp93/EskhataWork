a88c66eb17031ea6324ccb87515e58a5e6d708b5
RomanovSV <slava@litiko.com>
2025-09-03 20:06:15 +0300

    Countractual document cards 1.0 [Rx]

--- litiko.Eskhata/litiko.Eskhata.Server/Contract/ContractServerFunctions.cs
+++ litiko.Eskhata/litiko.Eskhata.Server/Contract/ContractServerFunctions.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
 using Sungero.CoreEntities;
-using litiko.NSI.ResponsibilityMatrix;
+using litiko.Eskhata.Contract;
 
-namespace litiko.NSI.Server
+namespace litiko.Eskhata.Server
 {
-  partial class ResponsibilityMatrixFunctions
+  partial class ContractFunctions
   {
   }
 }
