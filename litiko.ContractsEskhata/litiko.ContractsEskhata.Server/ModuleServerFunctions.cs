df7b543b7b9c33ebe9f2da707060b97d03e9489d
Bakha <63169312+BahodurTursunov@users.noreply.github.com>
2025-09-30 15:07:39 +0500

    изменения [Rx]

--- litiko.ContractsEskhata/litiko.ContractsEskhata.Server/ModuleServerFunctions.cs
+++ litiko.ContractsEskhata/litiko.ContractsEskhata.Server/ModuleServerFunctions.cs
@@ -14,26 +14,26 @@ namespace litiko.ContractsEskhata.Server
 {
   public class ModuleFunctions
   {
-    [Public(WebApiRequestType = RequestType.Post)]
-    public string ResponseToGetCountries(/*IIntegrationMethod method, IExchangeDocument exchDoc, long lastId*/)
-    {
-      var requestUrl = "10.10.2.53:9850";
-      
-      string applicationKey = "172.20.70.75:80/Integration/Odata/Integration/ProcessResponseFromIS##";
-      
-      var xmlRequestBody = string.Empty;
-
-      using (HttpClient client = new HttpClient())
-      {
-        client.DefaultRequestHeaders.Accept.Clear();
-        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
-
-        StringContent content = new StringContent(responseXML, Encoding.UTF8, "application/xml");
-
-        HttpResponseMessage response = client.PostAsync(requestUrl, responseXML).Result;
-        response.EnsureSuccessStatusCode();
-
-      }
-    }
+//    [Public(WebApiRequestType = RequestType.Post)]
+//    public string ResponseToGetCountries(/*IIntegrationMethod method, IExchangeDocument exchDoc, long lastId*/)
+//    {
+//      var requestUrl = "10.10.2.53:9850";
+//      
+//      string applicationKey = "172.20.70.75:80/Integration/Odata/Integration/ProcessResponseFromIS##";
+//      
+//      var xmlRequestBody = string.Empty;
+//
+//      using (HttpClient client = new HttpClient())
+//      {
+//        client.DefaultRequestHeaders.Accept.Clear();
+//        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+//
+//        StringContent content = new StringContent(responseXML, Encoding.UTF8, "application/xml");
+//
+//        HttpResponseMessage response = client.PostAsync(requestUrl, responseXML).Result;
+//        response.EnsureSuccessStatusCode();
+//
+//      }
+//    }
   }
 }
--- litiko.ContractsEskhata/litiko.ContractsEskhata.Shared/ModuleStructures.cs
+++ litiko.ContractsEskhata/litiko.ContractsEskhata.Shared/ModuleStructures.cs
@@ -1,10 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
 using Sungero.Core;
 using Sungero.CoreEntities;
+using Sungero.Docflow.Contracts; // Для работы с договорами
+using Sungero.Parties;           // Для работы с контрагентами
+using Sungero.Company;           // Для работы с сотрудниками
 
 namespace litiko.ContractsEskhata.Structures.Module
 {
+
+  /// <summary>
+  /// 
+  /// </summary>
+  partial class ContractData
+  {
+    [XmlElement("ContractNumber")]
+    public string ContractNumber {get; set;}
+    
+    [XmlElement("ContractDate")]
+    public string ContractDate { get; set; }
+    
+    [XmlElement("Amount")]
+    public decimal Amount { get; set; }
+
+    [XmlElement("CurrencyAlphaCode")]
+    public string CurrencyAlphaCode { get; set; }
+
+    [XmlElement("CounterpartyInn")]
+    public string CounterpartyInn { get; set; }
+
+    [XmlElement("CounterpartyName")]
+    public string CounterpartyName { get; set; }
+  }
 
 }
