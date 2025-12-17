67f3d3955fd1f8d215cdf6771b2a0316661dadae
RomanovSV <slava@litiko.com>
2025-12-17 18:35:19 +0200

    Перенос справочников модуля Договоры v1 [Rx]

--- litiko.Eskhata/litiko.Eskhata.ClientBase/DocumentKind/DocumentKindActions.cs
+++ litiko.Eskhata/litiko.Eskhata.ClientBase/DocumentKind/DocumentKindActions.cs
@@ -7,4 +7,14 @@ using litiko.Eskhata.DocumentKind;
 
 namespace litiko.Eskhata.Client
 {
+  partial class DocumentKindCollectionBulkActions
+  {
+
+    public virtual void AddToMappinglitiko(Sungero.Domain.Client.ExecuteActionArgs e)
+    {      
+      var result = NSI.PublicFunctions.Module.Remote.AddToMapping(NSI.Mapping.EntityType.DocumentKind.Value, _objIds.ToList());
+      Dialogs.ShowMessage(result);
+    }
+  }
+
 }
